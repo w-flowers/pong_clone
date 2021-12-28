99753b4903ec606f98f4475bc3a075993f9758d3
w-flowers <wig.flowers@gmail.com>
2021-12-28 12:15:17 +1100

    Began implementation of config file parsing
    - Defined file format for configurations
    - Defined member functions to read configuration file, and then
    parse line by line, resulting in either a valid field configuration
    or an InvalidConfig exception
    - Still need to define parsing rules for config lines specifying
    ball and line initialisation information. Further, as Paddle is not
    fully implemented yet, config will need to be modified to meet its
    needs.

--- inc/config.hpp
+++ inc/config.hpp
@@ -13,6 +13,8 @@
 
 #define PONG_CONFIG
 
+#include <iostream>
+
 enum Gametype { sim, multiplayer };
 
 struct Ball_Args
@@ -41,19 +43,34 @@ struct Configuration
 {
    Gametype gametype {sim};
 
-   int window_width {};
+   int window_width {1000};
 
-   int window_height {};
+   int window_height {800};
 
    std::vector< Ball_Args > balls {};
 
    std::vector< Line_Object_Args > lines {};
+
+   int gametype_set_count {0};
+
+   int dimensions_set_count {0};
 };
 
 // Exception for if read_config_file is fed bad input
 class InvalidConfig {};
 
-void read_config_file( Configuration& config, std::string filepath );
+void run_configuration( Configuration& config, std::string filepath );
+
+void read_config_file( Configuration& config, std::ifstream input );
+
+void process_config_line( Configuration& config,
+                          std::vector< std::string >& tokens );
+
+void parse_gametype_line( Configuration& config,
+                         std::vector< std::string >& tokens );
+
+void parse_dimensions_line( Configuration& config,
+                         std::vector< std::string >& tokens );
 
 #endif
 
--- src/config.cpp
+++ src/config.cpp
@@ -1,5 +1,5 @@
 
-/********************************************************************************
+/*******************************************************************************
 * Pong Clone - Config (config.cpp) 
 *
 * Read in data from a configuration file. The file determines the number of
@@ -7,8 +7,90 @@
 * whether the code is a simulation or a game.
 *
 * Author: William Flowers
-********************************************************************************/
+*******************************************************************************/
 
 #include "../inc/config.hpp"
 
+void run_configuration( Configuration& config, std::string filepath )
+{
+   // Open file given by filepath
+   
+   // Read Configuration file - store output in Configuration struct
+}
 
+void read_config_file( Configuration& config, std:ifstream input )
+{
+   std::string line_buffer;
+
+   std::vector< std::string > tokens {};
+
+   while( std::getline( input, line_buffer ) )
+   {
+      std::istringstream line_stream( line_buffer );
+
+      std::string input_token;
+
+      while( line_stream >> input_token )
+      {
+         tokens.push_back( input_token );
+      }
+
+      process_config_line( config, tokens );
+   }
+}
+
+void process_config_line( Configuration& config,
+                          std::vector< std::string >& tokens )
+{
+   if( tokens.at(0) == "gametype" )
+   {
+      parse_gametype_line( config, tokens );
+   }
+
+   if( tokens.at(0) == "dimensions" )
+   {
+      parse_dimensions_line( config, tokens );
+   }
+}
+
+void parse_gametype_line( Configuration& config,
+                          std::vector< std::string >& tokens )
+{
+   if( tokens.length() != 2 || ++( config.gametype_set_count ) > 1 )
+   {
+      throw InvalidConfig{};
+   }
+
+   else if( tokens.at(1) == "sim" )
+   {
+      config.gametype = sim;
+   }
+
+   else if( tokens.at(1) == "multiplayer" )
+   {
+      config.gametype = multiplayer;
+   }
+
+   else
+   {
+      throw InvalidConfig{};
+   }
+}
+
+void parse_dimensions_line( Configuration& config,
+                          std::vector< std::string >& tokens )
+{
+   if( tokens.length() != 3 || ++( config.dimensions_set_count ) > 1 )
+   {
+      throw InvalidConfig{};
+   }
+
+   config.window_width = std::stoi( tokens.at(1) );
+
+   config.window_height = std::stoi( tokens.at(2) );
+
+   if( config.window_width <= 0 || config.window_height <= 0 )
+   {
+      throw InvalidConfig{};
+   }
+}
